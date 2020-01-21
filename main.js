e3504cc5161fdf0ff49a193e6f4849225fb17689
Cassie Durr <Cass0115@gmail.com>
2020-01-21 15:19:47 -0600

    potential case study

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+const caseStudy = document.querySelector('.case-study');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close-button');
+const project = document.querySelector('.project');
+
+function toggleModal(){
+    modal.classList.toggle('show-modal');
+    if (event.target === modal) {
+        project.classList.add('none');
+    }
+}
+
+function windowOnClick(event) {
+    if(event.target === modal){
+        toggleModal();
+    }
+}
+
+caseStudy.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
+
+
+
 
